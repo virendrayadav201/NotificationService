ebbbe89b2638fb4f173e1ed446c20938bbee27a6
Virendra Yadav <tyadav492@gmail.com>
2025-10-04 22:53:48 +0530

    updated notification repository

--- NotificationService.Application/DTOs/NotificationDTO.cs
+++ NotificationService.Application/DTOs/NotificationDTO.cs
@@ -8,5 +8,16 @@ namespace NotificationService.Application.DTOs
 {
     public class NotificationDTO
     {
+        public int NotificationId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Message { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTime? SentAt { get; set; }
+
+        public string Status { get; set; }
     }
 }
--- NotificationService.Application/Services/Implementations/NotificationAppService.cs
+++ NotificationService.Application/Services/Implementations/NotificationAppService.cs
@@ -1,6 +1,8 @@
-﻿using NotificationService.Application.DTOs;
+﻿using AutoMapper;
+using NotificationService.Application.DTOs;
 using NotificationService.Application.Repositories;
 using NotificationService.Application.Services.Abstractions;
+using NotificationService.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,33 +14,36 @@ namespace NotificationService.Application.Services.Implementations
     public class NotificationAppService : INotificationAppService
     {
         private INotificationRepository _repository;
-        public NotificationAppService(INotificationRepository repository)
+        private IMapper _mapper;
+        public NotificationAppService(INotificationRepository repository, IMapper mapper)
         {
             _repository= repository;
+            _mapper= mapper;
         }
         public bool DeleteNotification(int id)
         {
-            throw new NotImplementedException();
+            return _repository.DeleteNotification(id);
         }
 
         public NotificationDTO GetNotification(int id)
         {
-            throw new NotImplementedException();
+            var result = _repository.GetNotification(id);
+            return _mapper.Map<NotificationDTO>(result);
         }
 
         public List<NotificationDTO> GetNotifications(int userId)
         {
-            throw new NotImplementedException();
+            return _mapper.Map<List<NotificationDTO>>(_repository.GetAllNotifications(userId));
         }
 
         public bool InsertNotification(NotificationDTO notification)
         {
-            throw new NotImplementedException();
+            return _repository.InsertNotification(_mapper.Map<Notification>(notification));
         }
 
         public bool UpdateNotification(NotificationDTO notification)
         {
-            throw new NotImplementedException();
+            return _repository.UpdateNotification(_mapper.Map<Notification>(notification));
         }
     }
 }
